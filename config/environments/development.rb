a1b652a372ea221457f0c90e488a25aee32159f7
Ubuntu <ubuntu@ip-172-31-63-9.ec2.internal>
2015-07-31 15:50:53 +0000

    sdfsf

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,16 +27,21 @@ Cenit::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   
-  config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+ # config.action_mailer.default_url_options = { :host => ENV['HOST'] }
+ # config.action_mailer.delivery_method = :smtp
+ # config.action_mailer.smtp_settings = {
+  #  :address              => "smtp.gmail.com",
+   # :port                 => 587,
+   # :domain               => 'cenitsaas.com',
+   # :user_name            => ENV['GMAIL_USERNAME'],
+   # :password             => ENV['GMAIL_PASSWORD'],
+   # :authentication       => 'plain',
+   # :enable_starttls_auto => true  }
+config.action_mailer.default_url_options = { :host => "localhost:3000"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    :address              => "smtp.gmail.com",
-    :port                 => 587,
-    :domain               => 'cenitsaas.com',
-    :user_name            => ENV['GMAIL_USERNAME'],
-    :password             => ENV['GMAIL_PASSWORD'],
-    :authentication       => 'plain',
-    :enable_starttls_auto => true  }
+      :address              => "localhost",
+      :port                 => 1025     }
   
 end
 
